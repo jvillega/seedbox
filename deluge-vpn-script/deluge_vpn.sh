bba63dd646f81f3b574473c2198a33e152e66045
seeder <seeder@seedbox-dev.villegascentral.com>
2025-03-04 16:17:45 -0800

    VPN Auth with File

--- deluge-vpn-script/deluge_vpn.sh
+++ deluge-vpn-script/deluge_vpn.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#
-sudo openvpn --config PATH_TO_VPN_USER_FILE --script-security 2 --up link_up_user_filter.sh
+
+sudo openvpn --config PATH_TO_VPN_USER_FILE --auth-user-pass .vpncredentials.txt --script-security 2 --up link_up_user_filter.sh
